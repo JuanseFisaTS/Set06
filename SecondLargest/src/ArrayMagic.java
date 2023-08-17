d040fb1483c2f71a7d643b64285ed165d810d22e
JuanseFisaTS <142258092+JuanseFisaTS@users.noreply.github.com>
2023-08-17 18:33:38 -0500

    Segundo mas grande

---
+++ SecondLargest/src/ArrayMagic.java
@@ -0,0 +1,28 @@
+public class ArrayMagic {
+ 
+    public int findSecondLargestElement(int[] array) {
+ 
+        if (array.length < 2) {
+            return -1;  
+        }
+ 
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+ 
+        for (int element : array) {
+            if (element > largest) {
+                secondLargest = largest;
+                largest = element;
+            } else if (element > secondLargest && element != largest) {
+                secondLargest = element;
+            }
+        }
+ 
+  
+        if (secondLargest == Integer.MIN_VALUE) {
+            return -1;
+        }
+ 
+        return secondLargest;
+    }
+}
